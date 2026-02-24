2c36127fd4e9eba931b1343b0b771f1e277309ec
Preslav Vuchev <p.uchev@expat.bg>
2026-02-24 15:12:49 +0200

    add gitignore

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np 
+
 
 def main():
 
     print("test")
     
-
-
-
 
 if __name__ == "__main__":
     main()
