95258b7ed5e5744c64c0f3de4ff184b5a085d2bb
Preslav Vachev <pres.vachev@gmail.com>
2026-02-25 14:38:49 +0200

    read

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
+from hmmlearn import hmm 
+
+np.random.seed(42)
 
 
 def main():
 
-    print("test")
+
+    df = pd.read_csv("GSPC_BND.csv",
+                     header=0,
+                     names=["Date",
+                            "BND",
+                            "GSPC",
+                            "LogReturnBND",
+                            "LogReturnGSPC"],
+                            parse_dates=True)
+
+    # print(df)
+
+    print(df)
+
+    # model = hmm.GaussianHMM(n_components=2,
+    #                         )
+
     
 
 if __name__ == "__main__":
